2ffe97ea9f7981f1cd37bbc81f9909da9f72da7a
bsee25007 <bsee25007@itu.edu.pk>
2026-03-30 00:10:29 +0500

    commit -done

--- functions.cpp
+++ functions.cpp
@@ -3,10 +3,39 @@
 
 int sumOfSquares(int n) 
 {
+
+    int r;
+
+if(n==1){
+
+    r = 1;  // base case when n is 1
+
+}
+
+else {
+
+    r = (n*n) + sumOfSquares(n-1);  //call function again for n-1
+
+}
+
+return r;  // return result
 }
 
 int findFactorial(int n)
 {
+
+
+    if(n<=1){
+
+    return 1;  // factorial of 0 or 1 is 1
+
+}
+
+else{
+
+    return n * findFactorial(n-1);  
+
+}
 }
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,38 @@
 
 
 int main() {
+
+
+
+
+    int c;   // making menu
+
+cout<<"Enter : "<<endl;
+cout<<"1 for Sum Of Squares "<<endl;
+cout<<"2 for Factorial "<<endl;
+cout<<"Enter : ";
+cin>>c;
+
+if(c==1){
+    int n;
+    cout<<"Enter number :"<<endl;
+    cin>>n;
+
+    cout<<"Sum Of Squares : "<<sumOfSquares(n)<<endl;  // shows result 
+}
+
+else if (c==2){
+
+    int n;
+    cout<<"Enter number :"<<endl;
+    cin>>n;
+
+    cout<<"Factorial = "<< findFactorial(n)<<endl;  // shows factorial result
+}
+
+else{
+
+    cout<<"Invalid input "<<endl;  // if user enters wrong choice
+}
    
 }
